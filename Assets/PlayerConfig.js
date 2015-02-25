770cc6a53f5ae350165a39a3789b4b4cd00c78dd
Wesley Schneider <octopusremote@gmail.com>
2015-02-24 23:37:52 -0300

    Cena de testes do attract

---
+++ Assets/PlayerConfig.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+public static var qdtParticleAtraidas = 0;
+public static var qdtMaxParticleAtrair = 4;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(objColidiu: Collider2D) {
+	if (objColidiu.gameObject.tag == "green"){
+	qdtParticleAtraidas =qdtParticleAtraidas+1;
+	
+	}
+	else if (objColidiu.gameObject.tag == "red"){
+	qdtParticleAtraidas =qdtParticleAtraidas-1;
+	
+	}
+	
+}
