776ce158a50a985f73a9117f3e8fd095402cb769
Wesley Schneider <octopusremote@gmail.com>
2015-02-26 15:54:26 -0300

    CENA TESTE - nesta cena tem o upgrade de mecânica: - follow dos Nodes quando toca - efeitos dinâmicos de contato - Previsão de todas possibilidades de vitória e derrota
    - Em JavaScript(não sei até quanto isso pode ser prejudicial pro projeto, mas é a linguagem q estou familiarizado no unity)

---
+++ Assets/AttractGREEN.js
@@ -0,0 +1,100 @@
+﻿#pragma strict
+
+public var speedMagnetic : Vector2;
+public var Suaviza = 0.1;
+public var ObjetoColidido : GameObject;
+public var atrair : boolean;
+public var jaColidiu : boolean;
+
+
+public var objSinal : GameObject;
+//private var myScript : Script;
+
+public var DistanceToPlayer :Vector2;
+
+
+
+function Start () {
+atrair = false;
+jaColidiu = false;
+
+}
+
+function Update () {
+
+	var DistanceToPlayer = Vector2.Distance(ObjetoColidido.gameObject.transform.position, gameObject.transform.position);
+
+	if(atrair==true){
+		Atracao();
+	}
+	else if (atrair==false){
+		//print('Off!!');
+	}
+	
+	if (DistanceToPlayer>3){
+		atrair=true;
+		(gameObject.GetComponent( "UpDown" ) as MonoBehaviour).enabled = false;
+		
+	}
+	else{
+		atrair=false;
+	}
+	
+
+
+//objColidiu = player1
+//gameObject = particle
+}
+
+function OnTriggerEnter2D(objColidiu: Collider2D) {
+//Player1
+	if (jaColidiu==false)
+	{
+		if (objColidiu.gameObject.tag == "Player1" && AttractPlayerConfig.qdtParticleAtraidasP1 < AttractPlayerConfig.qdtMaxParticleAtrair)
+		{
+			AttractPlayerConfig.qdtParticleAtraidasP1 =AttractPlayerConfig.qdtParticleAtraidasP1+1;
+			Suaviza=Random.Range(0.1,0.4);
+			print (AttractPlayerConfig.qdtParticleAtraidasP1);
+
+				jaColidiu = true;
+				gameObject.tag = 'greenColididoP1';
+				ObjetoColidido = objColidiu.gameObject;
+				gameObject.collider2D.isTrigger=false;
+				var sinal1 = Instantiate (objSinal, gameObject.transform.position, gameObject.transform.rotation);
+				yield WaitForSeconds (1);
+				Destroy(sinal1);
+
+
+		}
+		//Player2
+		if (objColidiu.gameObject.tag == "Player2" && AttractPlayerConfig.qdtParticleAtraidasP2 < AttractPlayerConfig.qdtMaxParticleAtrair)
+		{
+			AttractPlayerConfig.qdtParticleAtraidasP2 =AttractPlayerConfig.qdtParticleAtraidasP2+1;
+			Suaviza=Random.Range(0.1,0.4);
+			print (AttractPlayerConfig.qdtParticleAtraidasP2);
+			
+
+				jaColidiu = true;
+				gameObject.tag = 'greenColididoP2';
+				ObjetoColidido = objColidiu.gameObject;
+				gameObject.collider2D.isTrigger=false;
+				var sinal2 = Instantiate (objSinal, gameObject.transform.position, gameObject.transform.rotation);
+				yield WaitForSeconds (1);
+				Destroy(sinal2);
+
+		}
+		
+		
+		}
+
+}
+
+
+
+function Atracao(){
+
+	gameObject.transform.position.x = Mathf.SmoothDamp(gameObject.transform.position.x, ObjetoColidido.transform.position.x, speedMagnetic.x, Suaviza);
+	gameObject.transform.position.y = Mathf.SmoothDamp(gameObject.transform.position.y, ObjetoColidido.transform.position.y, speedMagnetic.y, Suaviza);
+
+	
+}
---
+++ Assets/AttractRED.js
@@ -0,0 +1,68 @@
+﻿#pragma strict
+
+
+public var ObjetoColidido : GameObject;
+public var jaColidiu : boolean;
+public var objSinal : GameObject;
+public static var GreenNodesScreenP1 : GameObject[];
+public static var GreenNodesScreenP2 : GameObject[];
+
+
+function Start () {
+jaColidiu = false;
+
+}
+
+function Update () {
+
+	GreenNodesScreenP1 = GameObject.FindGameObjectsWithTag("greenColididoP1");
+	GreenNodesScreenP2 = GameObject.FindGameObjectsWithTag("greenColididoP2");
+
+
+
+//objColidiu = player1
+//gameObject = particle
+}
+
+
+//Player1
+function OnTriggerEnter2D(objColidiu: Collider2D) {
+
+	if (jaColidiu==false){
+
+		if (objColidiu.gameObject.tag == "Player1"){
+			jaColidiu = true;
+			Destroy(gameObject);
+			
+			var sinal1 = Instantiate (objSinal, gameObject.transform.position, gameObject.transform.rotation);
+			AttractPlayerConfig.qdtParticleAtraidasP1 =AttractPlayerConfig.qdtParticleAtraidasP1-1;
+			Destroy(GreenNodesScreenP1[0]);
+			yield WaitForSeconds (1);
+			Destroy(sinal1);
+	
+
+	}
+	}
+	//Player2
+	if (jaColidiu==false){
+
+		if (objColidiu.gameObject.tag == "Player2"){
+			jaColidiu = true;
+			Destroy(gameObject);
+
+			var sinal2 = Instantiate (objSinal, gameObject.transform.position, gameObject.transform.rotation);
+			AttractPlayerConfig.qdtParticleAtraidasP2 =AttractPlayerConfig.qdtParticleAtraidasP2-1;
+			Destroy(GreenNodesScreenP2[0]);
+			yield WaitForSeconds (1);
+			Destroy(sinal2);
+			
+			
+
+
+		}
+	}
+}
+
+
+
+
