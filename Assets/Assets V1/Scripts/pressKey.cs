51b31b66a29fdf495ca85cb7d590cdd17d8b6e59
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-03-17 22:12:56 -0400

    Menu Principal 3D Dinamico em função da resolução e função de clicar para cortar a animação inicial do menu 3D.

--- Assets/Assets V1/Scripts/pressKey.cs
+++ Assets/Assets V1/Scripts/pressKey.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class pressKey : MonoBehaviour {
+public class pressKey : MonoBehaviour 
+{
+	public Animator cameraAnimator;
+	public Animator tituloAnimator;
 
+	public GameObject[] desactiveGameobjects;
 
-	// Update is called once per frame
 	void Update () 
 	{
-//		Debug.Log ("update");
 		if (Input.anyKeyDown) 
 		{
-			Application.LoadLevel ("VersusScene");		
+			if(cameraAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "CamIntroAnima_Nomes")
+			{
+				cameraAnimator.SetTrigger("Jump");
+				tituloAnimator.SetTrigger("Jump");
+
+				foreach(GameObject desactiveGameobject in desactiveGameobjects)
+					desactiveGameobject.SetActive(false);
+
+				GameObject[] guis = GameObject.FindGameObjectsWithTag ("GUI");
+
+				guis[0].GetComponent<Animator>().enabled = true;
+				guis[1].GetComponent<Animator>().enabled = true;
+			}
 		}
 	}
 }
