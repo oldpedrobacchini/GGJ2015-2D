984cf3704bb1cbbecb81196a3c95c039d426112e
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2015-03-23 17:44:06 -0400

    Atualização completa da organização do projeto e exclusão dos itens que não estavam sendo usados!

---
+++ Assets/01 MainMenu3D/Scripts/ActiveAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveAnimation : MonoBehaviour 
+{
+	public Animator cameraAnimator;
+
+	public void VersusAnimation()
+	{
+		cameraAnimator.SetTrigger("JumpVersus");
+	}
+}
---
+++ Assets/01 MainMenu3D/Scripts/JumpApresentation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpApresentation : MonoBehaviour 
+{
+	public Animator cameraAnimator;
+	public Animator tituloAnimator;
+
+	public GameObject[] desactiveGameobjects;
+
+	void Update () 
+	{
+		if (Input.anyKeyDown) 
+		{
+			if(cameraAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "CameraNomes")
+			{
+				cameraAnimator.SetTrigger("JumpMenu");
+				tituloAnimator.SetTrigger("JumpMenu");
+
+				foreach(GameObject desactiveGameobject in desactiveGameobjects)
+					desactiveGameobject.SetActive(false);
+
+				GameObject[] guis = GameObject.FindGameObjectsWithTag ("GUI");
+
+				guis[0].GetComponent<Animator>().enabled = true;
+				guis[1].GetComponent<Animator>().enabled = true;
+			}
+		}
+	}
+}
