a1730a24283f9d89c5002769b6586595e68b1779
WesleySchneider <octopusremote@gmail.com>
2015-04-13 14:40:30 -0300

    Novo Buraco Negro

--- Assets/02 Versus/Scripts/BlackHole.cs
+++ Assets/02 Versus/Scripts/BlackHole.cs
@@ -6,6 +6,18 @@ public class BlackHole : MonoBehaviour
 	public Match match;
 	public GameObject lastSignalPrefab;
 	public BehaviorUpDown[] moviments;
+	public GameObject giraBlackHole1;
+	public GameObject giraBlackHole2;
+	public GameObject giraBlackHole3;
+	public float speedGiroBlackHole1= -50f;
+	public float speedGiroBlackHole2 = 50f;
+	public float speedGiroBlackHole3 = 50f;
+
+	void Update () {
+		giraBlackHole1.transform.Rotate (new Vector3(0,0, Time.deltaTime*speedGiroBlackHole1));
+		giraBlackHole2.transform.Rotate (new Vector3(0,0, Time.deltaTime*speedGiroBlackHole2));
+		giraBlackHole3.transform.Rotate (new Vector3(0,0, Time.deltaTime*speedGiroBlackHole3));
+	}
 
 	public void StopBlackHole()
 	{
